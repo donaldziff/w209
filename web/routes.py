8eef7e92ee50c64898893944f4e4d9147fc0b368
Max Ziff <max.ziff@concur.com>
2021-03-28 11:40:03 -0700

    experimental inclusion of rong's charts

--- web/routes.py
+++ web/routes.py
@@ -18,40 +18,73 @@ def plot_all_category_global():
     df = utils.clean_data(df)
 
 
-    #########################################
-    # ploting - AMZN Product Data ScatterPlot
-    #########################################
+    ## to drop off outliers, choose data that has 'Est_Monthly_Sales' <1500 & 'Reviews' <200)
+    source = df.loc[(df['Est_Monthly_Sales'] <1500) & (df['Reviews'] <200)]
 
-    brush = alt.selection_interval()
+    brush = alt.selection(type='interval', resolve='global')
 
-    input_dropdown = alt.binding_select(options=["Arts, Crafts & Sewing", "Automotive", "Baby", "Beauty & Personal Care",  "Clothing, Shoes & Jewelry", "Health & Household", "Home & Kitchen", "Kitchen & Dining", "Musical Instruments", "Patio, Lawn & Garden", "Pet Supplies", "Sports & Outdoors", "Tools & Home Improvement", "Toys & Games", "Video Games"], name="Select a category..." )
-
-    selection = alt.selection_single(fields=['Category'], bind=input_dropdown, clear=alt.EventStream(type='dblclick'))
-
-    points = alt.Chart(df).mark_circle().encode(
-        x='Est_Monthly_Sales:Q',
-        y='Est_Monthly_Revenue:Q',
-        color = alt.condition(selection,
-                                alt.Color('Category:N', legend=None),
-                                alt.value('lightgray')),
-        tooltip=['Sellers', 'LQS', 'Reviews', 'Rank', 'Fees', 'Net', 'Est_Monthly_Sales','Est_Monthly_Revenue', 'Category', 'Product_Name']
-    ).properties(width=1000, height=500).add_selection(
-        brush
+    ## chart1-High Demand and Low Competion
+    chart1 = alt.Chart(source,title="High Demand and Low Competion").transform_calculate(
+        url='https://www.amazon.com/dp/' + alt.datum.ASIN
+    ).mark_circle(size=60).encode(
+        x='Reviews',
+        y = alt.Y('Est_Monthly_Sales', scale=alt.Scale(domain=[0, 1600])),
+        color=alt.condition(brush, 'ASIN:O', alt.ColorValue('whitesmoke'), legend=None),
+        tooltip=['Product_Name','url:N','Est_Monthly_Sales','Category','Reviews', 'LQS', 'Net','Price',]
     ).add_selection(
-        selection
-    ).transform_filter(
-        selection)
+        brush).properties(
+        height=400,
+        width=400)
 
-    bars = alt.Chart(df).mark_bar().encode(
-        y='Category:N',
-        color='Category:N',
-        x='count(Category):Q'
-    ).properties(width=1000, height=200).transform_filter(
-        brush
-    )
+    ## chart2 - High Demand and Bad Marketing
+    chart2 = alt.Chart(source,title="High Demand and Bad Marketing").mark_circle(size=60).encode(
+        x='LQS',
+        y = alt.Y('Est_Monthly_Sales', scale=alt.Scale(domain=[0, 1600])),
+        color=alt.condition(brush, 'ASIN:O', alt.ColorValue('whitesmoke'), legend=None),
+        tooltip=['Product_Name','ASIN','Est_Monthly_Sales','Category','Reviews', 'LQS', 'Net','Price',]
+    ).add_selection(
+        brush).properties(
+        height=400,
+        width=400)
 
-    plot_product_scatterchart =  points & bars
+    ## chart3 - Good Return on Investment  
+    chart3 = alt.Chart(source,title="Good Return on Investment").mark_circle(size=60).encode(
+        x='Price',
+        y='Net',
+        color=alt.condition(brush, 'ASIN:O', alt.ColorValue('whitesmoke'), legend=None),
+        tooltip=['Product_Name','ASIN','Est_Monthly_Sales','Category','Reviews', 'LQS', 'Net','Price',]
+    ).add_selection(
+        brush).properties(
+        height=400,
+        width=400)
 
+    ## chart4 - Good Demand & Poor Quality
+
+    chart4 = alt.Chart(source,title="Good Demand & Poor Quality").mark_circle(size=60).encode(
+        x='Rating',
+        y = alt.Y('Est_Monthly_Sales', scale=alt.Scale(domain=[0, 1600])),
+        color=alt.condition(brush, 'ASIN:O', alt.ColorValue('whitesmoke'), legend=None),
+        tooltip=['Product_Name','ASIN','Est_Monthly_Sales','Category','Reviews', 'LQS', 'Net','Price',]
+    ).add_selection(
+        brush).properties(
+        height=400,
+        width=400)
+
+    ## line1 Est_Monthly_Sales >= 200
+    line1 = alt.Chart(pd.DataFrame({'y': [200]})).mark_rule(color='red').encode(y='y')
+    ## line2 Reviews <=50
+    line2 = alt.Chart(pd.DataFrame({'x': [50]})).mark_rule(color='red').encode(x='x')
+    ## line3 LQS <6
+    line3 = alt.Chart(pd.DataFrame({'x': [6]})).mark_rule(color='red').encode(x='x')
+    ## line4 Price>20
+    line4 = alt.Chart(pd.DataFrame({'x': [20]})).mark_rule(color='red').encode(x='x')
+    ## line5 Net>15
+    line5 = alt.Chart(pd.DataFrame({'y': [15]})).mark_rule(color='red').encode(y='y')
+    ## line6 Rating<3.7
+    line6 = alt.Chart(pd.DataFrame({'x': [3.7]})).mark_rule(color='red').encode(x='x')
+    
+    plot_product_scatterchart =  chart1 + line1 + line2|chart2 + line1 + line3|chart3 + line5 +line4|chart4 + line1 + line6
+    
     plot_product_scatterchart_json = plot_product_scatterchart.to_json()
 
 
@@ -132,8 +165,6 @@ def plot_all_category_global():
                }
 
     return render_template('all_category.html', context=context )
-
-
 
 
 # Ivan changed code 2-3-2021
